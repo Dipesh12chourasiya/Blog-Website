06095229406f3ed92698d5a93aa6f828678e86b8
Dipesh <dipeshchourasiya12@gmail.com>
2024-08-04 16:25:18 +0530

    create blog section added

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import Layout from '../../components/layout/Layout'
+import HeroSection from '../../components/heroSection/HeroSection'
+import BlogPostCard from '../../components/blogPostCard/BlogPostCard'
+import Loader from '../../components/loader/Loader'
 
-const Home = () => {
+function Home() {
   return (
-    <div>
-      Home
-    </div>
+    <Layout>
+        <HeroSection/>
+        <BlogPostCard />
+        <Loader />
+    </Layout>
   )
 }
 
